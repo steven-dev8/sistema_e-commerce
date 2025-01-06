d4ab747a9d49323c3ac4437daed61c057600ee03
Carlos Araújo da Costa <stevenndev8@gmail.com>
2025-01-05 21:10:12 -0300

    Update

--- convert/convert.py
+++ convert/convert.py
@@ -2,10 +2,8 @@ from PIL import Image
 
 def convert_to_ppm(input_image_path, output_image_path):
     try:
-        # Abre a imagem com PIL
         img = Image.open(input_image_path)
 
-        # Converte a imagem para o formato PPM
         img.save(output_image_path, "PPM")
 
         print(f"Imagem convertida para {output_image_path} com sucesso!")
@@ -15,7 +13,7 @@ def convert_to_ppm(input_image_path, output_image_path):
 
 while True:
     diretorio = input('DIRETORIO: ')
-    input_image = f'{diretorio}.png'  # Caminho da imagem original
-    output_image = f'output\{diretorio}.ppm'  # Caminho da imagem convertida para PPM
+    input_image = f'{diretorio}.png'
+    output_image = f'output\{diretorio}.ppm'
 
     convert_to_ppm(input_image, output_image)
